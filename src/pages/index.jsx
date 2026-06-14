3f3fe4a181dea9804fdd0f591e436b72862463e9
dashon1 <dashon1@github.com>
2026-06-13 22:04:49 -0400

    add Login page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,6 +18,8 @@ import Reports from "./Reports";
 
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
+import Login from './Login';
+
 const PAGES = {
     
     Dashboard: Dashboard,
@@ -55,6 +57,10 @@ function _getCurrentPage(url) {
 function PagesContent() {
     const location = useLocation();
     const currentPage = _getCurrentPage(location.pathname);
+    if (/\/login$/i.test(location.pathname)) {
+        return <Routes><Route path="/login" element={<Login />} /><Route path="/Login" element={<Login />} /></Routes>;
+    }
+
     
     return (
         <Layout currentPageName={currentPage}>
